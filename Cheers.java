5ff547ca3ec6729467665031d030a5e3eabf2e73
galie-ezra <ggaliezra@gmail.com>
2024-11-14 21:39:18 +0200

    task 1-3

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,25 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String string = args[0];
+            int number = Integer.parseInt(args[1]);
+            String letters = "AEFHILMNOR";
+            
+            for(int i=0;i<string.length(); i++)
+            {
+                if (letters.indexOf(string.charAt(i))!= -1)
+                {
+                        System.out.println("Give me an " + string.charAt(i)+ ": "+ string.charAt(i)+"!");
+                }
+                else
+                {
+                        System.out.println("Give me a " + string.charAt(i)+ ": "+ string.charAt(i)+"!");
+                }
+            }
+            System.out.println("What does that spell?");
+            for (int i=0; i<number; i++)
+            {
+                System.out.println(string + " ! ! !");
+            }
         }
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,39 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        String time= args[0];
+        String minutesToAdd= args[1];
+
+        String minutes= "" + time.charAt(3)+ time.charAt(4);
+        String hours= "" + time.charAt(0)+ time.charAt(1);
+
+        int totalMinutes = (Integer.parseInt(hours)*60) +
+        Integer.parseInt(minutes) + Integer.parseInt(minutesToAdd);
+        int totalHours= totalMinutes/60;
+        int newHours= totalHours%24;
+        int newNinutes=totalMinutes- (totalHours*60);
+        String finalHours= String.valueOf(newHours);
+        String finalMinutes=String.valueOf(newNinutes);
+
+        if (newHours < 10)
+        {
+            finalHours= "0"+newHours;
+        }
+        if (newNinutes < 10)
+        {
+            finalMinutes= "0"+ newNinutes;
+        }
+
+        String FinalTime= finalHours + ":" + finalMinutes;
+        System.out.println(FinalTime);
+
+
+        /*System.out.println(hours);
+        System.out.println(minutes);
+        System.out.println(minutesToAdd);
+        System.out.println(totalMinutes);
+        System.out.println(totalHours);
+        System.out.println(newHours);
+        System.out.println(newNinutes);*/
+
     }
 }
