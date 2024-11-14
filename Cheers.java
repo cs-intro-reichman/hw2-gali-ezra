47d08e710340d6b72f4288097199b37f37c9b6ff
galie-ezra <ggaliezra@gmail.com>
2024-11-14 21:47:38 +0200

    fix 3

--- Cheers.java
+++ Cheers.java
@@ -2,6 +2,7 @@
 public class Cheers {
         public static void main(String[] args) {
 	    String string = args[0];
+            String upperCaseString= string.toUpperCase();
             int number = Integer.parseInt(args[1]);
             String letters = "AEFHILMNOR";
             
@@ -9,17 +10,17 @@ public class Cheers {
             {
                 if (letters.indexOf(string.charAt(i))!= -1)
                 {
-                        System.out.println("Give me an " + string.charAt(i)+ ": "+ string.charAt(i)+"!");
+                        System.out.println("Give me an " + upperCaseString.charAt(i)+ ": "+ upperCaseString.charAt(i)+"!");
                 }
                 else
                 {
-                        System.out.println("Give me a " + string.charAt(i)+ ": "+ string.charAt(i)+"!");
+                        System.out.println("Give me a " + upperCaseString.charAt(i)+ ": "+ upperCaseString.charAt(i)+"!");
                 }
             }
             System.out.println("What does that spell?");
             for (int i=0; i<number; i++)
             {
-                System.out.println(string + " ! ! !");
+                System.out.println(upperCaseString + "!!!");
             }
         }
 }
